05b92d9e5952a702c344fbfbf353afa2bb119243
kyokoyaho <kagikatsuko@gmail.com>
2021-06-16 01:38:39 +0900

    upボタン(上へボタン）を追加

--- app/src/main/java/com/android/example/helloapp/MainActivity.kt
+++ app/src/main/java/com/android/example/helloapp/MainActivity.kt
@@ -2,12 +2,28 @@ package com.android.example.helloapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 参考: https://developer.android.com/codelabs/kotlin-android-training-add-navigation?index=..%2F..android-kotlin-fundamentals#7
+        // navControllerオブジェクトを取得
+        // val navController = this.findNavController(R.id.nav_host_fragment) ←　runtime Exception
+        val navController = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)!!.findNavController()
+        // navControllerをアプリバーにリンクする
+        NavigationUI.setupActionBarWithNavController(this,navController)
+
         Timber.i("test timber")
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
     }
 }
